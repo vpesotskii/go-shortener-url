2be79f1742e6bd4ce5c865e2a3002251eceb8784
vpesotskii <vpesotsky@gmail.com>
2024-10-29 18:05:45 +0300

    fix vet autotest

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 )
 
-var mapUrls = map[string]string{}
+var mapURLs = map[string]string{}
 
+// func handles the request
 func handleMethod(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		addUrl(w, r)
+		addURL(w, r)
 	case http.MethodGet:
 		getUrl(w, r)
 	default:
@@ -20,24 +21,26 @@ func handleMethod(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func addUrl(res http.ResponseWriter, req *http.Request) {
+// func encodes the URL from the request and put it into the map
+func addURL(res http.ResponseWriter, req *http.Request) {
 
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
-	shortUrl := encodeUrl(body)
-	mapUrls[shortUrl] = string(body)
+	shortURL := encodeURL(body)
+	mapURLs[shortURL] = string(body)
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusCreated)
-	res.Write([]byte("http://localhost:8080/" + shortUrl))
+	res.Write([]byte("http://localhost:8080/" + shortURL))
 }
 
+// func returns the original URL by short URL
 func getUrl(res http.ResponseWriter, req *http.Request) {
-	shortUrl := req.URL.String()[1:]
-	if originalUrl, ok := mapUrls[shortUrl]; ok {
-		res.Header().Set("Location", originalUrl)
+	shortURL := req.URL.String()[1:]
+	if originalURL, ok := mapURLs[shortURL]; ok {
+		res.Header().Set("Location", originalURL)
 		res.WriteHeader(http.StatusTemporaryRedirect)
 	} else {
 		res.Header().Set("Location", "URL not found")
@@ -45,7 +48,8 @@ func getUrl(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func encodeUrl(url []byte) string {
+// func encodes string by base64
+func encodeURL(url []byte) string {
 	return base64.StdEncoding.EncodeToString(url)
 }
 
