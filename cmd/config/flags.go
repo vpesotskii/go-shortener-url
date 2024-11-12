05806bfcec256ef14c158c094a256ca85349d130
vpesotskii <vpesotsky@gmail.com>
2024-11-12 16:33:05 +0300

    add zap logger

--- cmd/config/flags.go
+++ cmd/config/flags.go
@@ -8,11 +8,13 @@ import (
 var Options struct {
 	Server      string
 	BaseAddress string
+	LogLevel    string
 }
 
 func ParseFlags() {
 	flag.StringVar(&Options.Server, "a", "localhost:8080", "address HTTP server")
 	flag.StringVar(&Options.BaseAddress, "b", "http://localhost:8080", "Base address")
+	flag.StringVar(&Options.LogLevel, "l", "info", "Log level")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
@@ -20,5 +22,8 @@ func ParseFlags() {
 	}
 	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
 		Options.BaseAddress = envBaseAddr
+	}
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		Options.LogLevel = envLogLevel
 	}
 }
